64f4463c68c384df7ff6b4002fa561ac3cf6c0f2
Lancelot Owczarczak <lancelot@owczarczak.fr>
2024-05-05 14:10:31 +0900

    :zap: Add autocompletion

--- nvim/init.lua
+++ nvim/init.lua
@@ -57,6 +57,40 @@ require("lazy").setup({
 			vim.cmd.colorscheme("molokai")
 		end,
 	},
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+		},
+		config = function()
+			local cmp = require("cmp")
+			local luasnip = require("luasnip")
+			luasnip.config.setup({})
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						luasnip.lsp_expand(args.body)
+					end,
+				},
+				completion = { completeopt = "menu,menuone,noinsert" },
+
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+					["<C-Space>"] = cmp.mapping.complete({}),
+				}),
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+				},
+			})
+		end,
+	},
 })
 
 -- Setup LSP/Formatter manager
